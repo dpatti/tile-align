b9f5133226eeb4d014a20a49918440bfcde6ee6b
Doug Patti <douglas.patti@gmail.com>
2012-05-03 00:04:52 -0400

    Move elements to HTML

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 $(function(){
     var offset = 100;
     var scale = 1;
-    var orig_width = null;
-    var img = $("<img>").attr('id', 'static').attr('src', 'sample.png').load(function(){ orig_width = $(this).width(); });
-    var overlay = img.clone().attr('id', 'overlay').attr('draggable', false).css('left', offset);
-    var marker = $("<div>").attr('id', 'marker').css('left', offset).text(offset + "px");
+    var orig_width;
+    var img = $("#static").load(function(){ orig_width = $(this).width(); });
+    var overlay = $("#overlay").css('left', offset);
+    var marker = $("#marker").css('left', offset);
+    orig_width = img.width() || null;
     
-    $("#edit-area").append(img, overlay, marker);
-
     var load_image = function(url) {
         orig_width = null;
         scale = 1;
@@ -17,22 +16,20 @@ $(function(){
     }
 
     var marker_update = function(){
-        var str = offset + "px";
-        var appx_scale = Math.round(scale*100);
-        if (scale != 100)
-            str += " @ " + appx_scale + "%";
-        marker.text(str);
+        marker.find(".offset").text(offset);
+        marker.find(".scale").text(Math.round(scale*100));
     }
+    marker_update();
 
     var set_position = function(delta) {
-        offset = Math.min(img.width(), Math.max(0, offset + delta));
+        offset = Math.min(orig_width, Math.max(0, offset + delta));
         overlay.add(marker).css('left', offset * scale);
         marker_update();
     }
 
     var set_scale = function(delta) {
         if (orig_width) {
-            var imgs = $("#edit-area img");
+            var imgs = img.add(overlay);
             scale *= Math.pow(1.1, delta);
             imgs.width(orig_width * scale);
             set_position(0);
