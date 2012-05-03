76c6d3a7538c85b1a8bd96302fe98fba5ddd3ed5
Doug Patti <douglas.patti@gmail.com>
2012-05-02 23:49:25 -0400

    Add scaling

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 $(function(){
     var offset = 100;
-    var img = $("<img>").attr('id', 'static').attr('src', 'sample.png');
+    var scale = 1;
+    var orig_width = null;
+    var img = $("<img>").attr('id', 'static').attr('src', 'sample.png').load(function(){ orig_width = $(this).width(); });
     var overlay = img.clone().attr('id', 'overlay').attr('draggable', false).css('left', offset);
-    var marker = $("<div>").attr('id', 'marker').css('left', offset).text(offset);
+    var marker = $("<div>").attr('id', 'marker').css('left', offset).text(offset + "px");
     
     $("#edit-area").append(img, overlay, marker);
 
     var load_image = function(url) {
+        orig_width = null;
+        scale = 1;
+        offset = 100;
         img.add(overlay).attr('src', url);
+        set_position();
     }
 
-    var position = function(delta) {
+    var marker_update = function(){
+        var str = offset + "px";
+        var appx_scale = Math.round(scale*100);
+        if (scale != 100)
+            str += " @ " + appx_scale + "%";
+        marker.text(str);
+    }
+
+    var set_position = function(delta) {
         offset = Math.min(img.width(), Math.max(0, offset + delta));
-        overlay.add(marker).css('left', offset);
-        marker.text(offset);
+        overlay.add(marker).css('left', offset * scale);
+        marker_update();
+    }
+
+    var set_scale = function(delta) {
+        if (orig_width) {
+            var imgs = $("#edit-area img");
+            scale *= Math.pow(1.1, delta);
+            imgs.width(orig_width * scale);
+            set_position(0);
+        }
     }
 
     // Interface
@@ -28,14 +51,18 @@ $(function(){
     
     // Key
     $(document).keydown(function(e){
+        if (e.keyCode >= 37 && e.keyCode <= 40)
+            e.preventDefault();
+
         var shamt = e.shiftKey ? 10 : 1;
-        if (e.keyCode == 37) {
-            position(-shamt);
-            e.preventDefault();
-        } else if (e.keyCode == 39) {
-            position(shamt);
-            e.preventDefault();
-        }
+        if (e.keyCode == 37)
+            set_position(-shamt);
+        else if (e.keyCode == 39)
+            set_position(shamt);
+        else if (e.keyCode == 38)
+            set_scale(-1);
+        else if (e.keyCode == 40)
+            set_scale(+1);
     });
 
     // Dragging
@@ -45,8 +72,7 @@ $(function(){
     });
     $(document).mousemove(function(e){
         if (start_x != null) {
-            position(e.pageX - start_x);
-            console.log(e.pageX - start_x);
+            set_position(e.pageX - start_x);
             start_x = e.pageX;
         }
     }).mouseup(function(){
